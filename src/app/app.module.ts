6d92ce94837324301a8c33a57703a1a462d576fa
umt.eng52@gmail.com <umt.eng52@gmail.com>
2023-01-12 14:38:00 +0700

    install ngBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule }from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { 
+  NgbAlertModule,
+  NgbDropdown, 
+  NgbModule 
+} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule,
+    NgbDropdown,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
