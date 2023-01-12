e346b64e8cb7ccba16bb2949d39e9294d208c594
umt.eng52@gmail.com <umt.eng52@gmail.com>
2023-01-12 16:10:52 +0700

    xxx

--- src/app/components/vaccine-import/vaccine-import.component.ts
+++ src/app/components/vaccine-import/vaccine-import.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import readXlsxFile from 'read-excel-file';
 
 @Component({
   selector: 'app-vaccine-import',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./vaccine-import.component.css']
 })
 export class VaccineImportComponent {
+  constructor(){ }
+
+  result:any[] = [];
+
+  excelRead(e:any){
+    let fileReaded:any;
+    fileReaded = e.target.files[0];
+    let type = e.target.files[0].name.split('.').pop();
+
+    const schema = {
+      'prefix': {        
+        prop: 'prefix',
+        type: String
+      },
+      'first_name': {
+        prop: 'first_name',
+        type: String
+      },
+      'last_name': {
+        prop: 'last_name',
+        type: String
+      }
+      
+    };
+
+    readXlsxFile(e.target.files[0], {schema}).then((data:any)=>{     
+      console.log(data);
+       
+      if(data.rows){
+        for(let i of data.rows){
+          this.result.push(i);
+        }
+      }
+      console.log(this.result);      
+    })
+  }
 
 }
